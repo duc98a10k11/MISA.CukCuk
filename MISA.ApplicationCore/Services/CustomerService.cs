7c0183c8c54b8ba14c0ad252b5af1ed037bc0134
DUC LE MINH <lmduc@rd.local>
2021-04-20 10:00:18 +0700

    update code kiến trúc ứng dụng

---
+++ MISA.ApplicationCore/Services/CustomerService.cs
@@ -0,0 +1,38 @@
+﻿using MISA.ApplicationCore.Entities;
+using MISA.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.ApplicationCore.Services
+{
+    public class CustomerService : ICustomerRepository
+    {
+        public int AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCustomerById(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EditCustomer(Customer customer, Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomerById(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MISA.Infrastructure/Data/CustomerReponsitory.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using MISA.ApplicationCore.Entities;
+using MISA.ApplicationCore.Interfaces;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.Infrastructure.Data
+{
+    class CustomerReponsitory : ICustomerRepository
+    {
+        public int AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCustomerById(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EditCustomer(Customer customer, Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetAll()
+        {
+            //1. Khởi tạo Connection
+            string connectionString = "" +
+               "Host = 47.241.69.179;" +
+               "Port = 3306;" +
+               "User Id = dev;" +
+               "Password = 12345678;" +
+               "Database = MF0_NVManh_CukCuk02;";
+            //2. Kết nối connection
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            //3. Lấy dữ liệu từ DB
+
+            var customers = dbConnection.Query<Customer>("Proc_GetCustomers", commandType: CommandType.StoredProcedure);
+            return (Customer)customers;
+        }
+
+        public Customer GetCustomerById(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
